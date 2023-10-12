5ae57e32bfe80865c60c22a8a3901eacb6f3fe1a
AyonJD <ayonjodder177@gmail.com>
2023-10-12 14:31:51 +0600

    Login solved

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,5 @@
 import './globals.css'
+import { Toaster } from 'react-hot-toast'
 
 export const metadata = {
   title: 'Create Next App',
@@ -9,6 +10,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>{children}</body>
+      <Toaster />
     </html>
   )
 }
